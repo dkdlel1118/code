09af346df4e21442b4f851ef21a502bbb925bb80
dkdlel1118 <kimgeunhui503@gmail.com>
2025-06-08 12:39:05 +0900

    Add files via upload

--- code/c code 파일/c code 파일/1code.c
+++ code/c code 파일/c code 파일/1code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>  // atoi, atof 함수 사용하기위한 라이브러리 stdlib.h
 
 int main() {
 	//1
@@ -62,6 +63,7 @@ int main() {
 	//	else {
 	//		printf("아쉽지만 다음 기회에");
 	//	}
+	//2번문제
 	//int a;
 	//	printf("이번 년도의 날짜는 어떻게 되시는가요--->");
 	//	scanf_s("%d", &a);
@@ -79,4 +81,110 @@ int main() {
 	//	else{
 	//		printf("평년임");
 	//	}
+	//7
+	//int myNum = 15;        // 선언 + 초기화
+	//int myNum;             // 선언만
+	//myNum = 15;            // 나중에 값 대입
+	//8
+	//int a = 15;
+	//float b = 30.4;
+	//char c ='k';
+	//char d[10] = "k.k.k.";
+	//printf("%d\n", a);
+	//printf("%f\n", b);
+	//printf("%c\n", c);
+	//printf("%s\n", d);
+	// 9
+	//float x = 3.5;
+	//int y = (int)x;  // x를 정수로 강제 변환 → y = 3
+	//printf("%d\n", y);
+	//10
+	//char strInt[] = "123";
+	//char strFloat[] = "3.14";
+	//int num = atoi(strInt);        // 문자열 → 정수
+	//float fnum = atof(strFloat);   // 문자열 → 실수
+	//printf("정수 변환: %d\n", num);
+	//printf("실수 변환: %f\n", fnum);
+	// 11
+	//char a[10]; 
+	//char b[10];
+	//int s;
+	//	scanf_s("%s", a, (unsigned)_countof(a));//a=10
+	//	scanf_s("%s", b, (unsigned)_countof(b));//b=20
+	//	int num = atoi(a);        // 문자열 → 정수
+	//	int nam = atoi(b);
+	//	printf("정수 변환: %d\n", num);
+	//	printf("정수 변환: %d\n", nam);
+	//	s = num + nam;
+	//	printf("%d\n",s);//30
+	//12
+	//for (int a = 10; a >= 1; a--) {
+	//	printf("%d\n", a);
+	//	}
+	// 13
+	//int b=0;
+	//for (int a = 2; a <= 100; a += 2) {
+	//	b = b + a;
+	//}
+	//printf("%d\n", b);
+	//문제 3
+	int a;
+	printf("'더하기는 1번, 빼기는 2번,나누기는 3번,곱하기는 4번입니다.입력하실 곳은 여기 -->'");
+	scanf_s("%d", &a);
+	if (a == 1) {
+		int x;
+		printf("첫번쨰 수를 알려주세요:");
+		scanf_s("%d", &x);
+		int y;
+		printf("두번쨰 수를 알려주세요:");
+		scanf_s("%d", &y);
+		int k;
+		k = x + y;
+		printf("%d\n", k);
+	}
+	else if (a == 2) {
+
+
+		int x;
+		printf("첫번쨰 수를 알려주세요:");
+		scanf_s("%d", &x);
+		int y;
+		printf("두번쨰 수를 알려주세요:");
+		scanf_s("%d", &y);
+		int k;
+		k = x - y;
+		printf("%d\n", k);
+	}
+	else if (a == 3) {
+
+
+		int x;
+		printf("첫번쨰 수를 알려주세요:");
+		scanf_s("%d", &x);
+		int y;
+		printf("두번쨰 수를 알려주세요:");
+		scanf_s("%d", &y);
+		int k;
+		k = x * y;
+		printf("%d\n", k);
+	}
+
+	else if (a == 4) {
+
+
+		int x;
+		printf("첫번쨰 수를 알려주세요:");
+		scanf_s("%d", &x);
+		int y;
+		printf("두번쨰 수를 알려주세요:");
+		scanf_s("%d", &y);
+		int k;
+		k = x / y;
+		printf("%d\n", k);
+	}
+	else {
+		printf("다시 입력해주세요");
+	}
+	
+	return 0;
 	}
